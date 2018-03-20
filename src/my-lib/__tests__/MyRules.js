1b738b7d9605a19b71fc92fd34e44e3a627094ca
William Goillot <william.goillot@gmail.com>
2018-03-20 10:06:36 +0100

    Unit testing for define victory

--- src/my-lib/__tests__/MyRules.js
+++ src/my-lib/__tests__/MyRules.js
@@ -1,25 +1,44 @@
 import MyRules          from '../MyRules';
 
-test('Get rules: classic', () => {
+test('Get rules', () => {
     expect(MyRules.getRules('classic')).toBeDefined();
-});
-
-test('Get rules: lizard-spock', () => {
     expect(MyRules.getRules('lizard-spock')).toBeDefined();
-});
-
-test('Get rules: invalid mode', () => {
     expect(MyRules.getRules('invalid')).toBeDefined();
 });
 
-test('Get element: invalid mode', () => {
+test('Get element', () => {
     expect(MyRules.getElement('invalid')).toBeDefined();
-});
-
-test('Get element: valid element', () => {
     expect(MyRules.getElement('lizard')).toBeDefined();
 });
 
+test('Define victory', () => {
+    expect(MyRules.defineVictory('classic', 'scissors', 'paper')).toBe(1);
+    expect(MyRules.defineVictory('classic', 'paper', 'rock')).toBe(1);
+    expect(MyRules.defineVictory('classic', 'rock', 'scissors')).toBe(1);
+    expect(MyRules.defineVictory('classic', 'paper', 'scissors')).toBe(-1);
+    expect(MyRules.defineVictory('classic', 'rock', 'paper')).toBe(-1);
+    expect(MyRules.defineVictory('classic', 'scissors', 'rock')).toBe(-1);
+    expect(MyRules.defineVictory('classic', 'paper', 'paper')).toBe(0);
+    expect(MyRules.defineVictory('classic', 'rock', 'rock')).toBe(0);
+    expect(MyRules.defineVictory('classic', 'scissors', 'scissors')).toBe(0);
+    expect(MyRules.defineVictory('lizard-spock', 'scissors', 'paper')).toBe(1);
+    expect(MyRules.defineVictory('lizard-spock', 'scissors', 'lizard')).toBe(1);
+    expect(MyRules.defineVictory('lizard-spock', 'paper', 'rock')).toBe(1);
+    expect(MyRules.defineVictory('lizard-spock', 'paper', 'spock')).toBe(1);
+    expect(MyRules.defineVictory('lizard-spock', 'rock', 'scissors')).toBe(1);
+    expect(MyRules.defineVictory('lizard-spock', 'rock', 'lizard')).toBe(1);
+    expect(MyRules.defineVictory('lizard-spock', 'paper', 'scissors')).toBe(-1);
+    expect(MyRules.defineVictory('lizard-spock', 'lizard', 'scissors')).toBe(-1);
+    expect(MyRules.defineVictory('lizard-spock', 'rock', 'paper')).toBe(-1);
+    expect(MyRules.defineVictory('lizard-spock', 'spock', 'paper')).toBe(-1);
+    expect(MyRules.defineVictory('lizard-spock', 'scissors', 'rock')).toBe(-1);
+    expect(MyRules.defineVictory('lizard-spock', 'lizard', 'rock')).toBe(-1);
+    expect(MyRules.defineVictory('lizard-spock', 'paper', 'paper')).toBe(0);
+    expect(MyRules.defineVictory('lizard-spock', 'rock', 'rock')).toBe(0);
+    expect(MyRules.defineVictory('lizard-spock', 'scissors', 'scissors')).toBe(0);
+    expect(MyRules.defineVictory('lizard-spock', 'spock', 'spock')).toBe(0);
+    expect(MyRules.defineVictory('lizard-spock', 'lizard', 'lizard')).toBe(0);
+});
 
 
 
